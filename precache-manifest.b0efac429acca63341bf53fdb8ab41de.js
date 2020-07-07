ee91a9637e8ac62264b6aed45a05a6d046bee085
saitejareddy yarrabothula <cb.sc.i5das18041@cb.students.amrita.edu>
2020-07-07 22:41:04 +0530

    Updates

---
+++ precache-manifest.b0efac429acca63341bf53fdb8ab41de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b94b9fa63a090bc6c4eedd3853ea1be",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "1422cd7d6188d79bd7b2",
+    "url": "/weather/static/css/main.1097af14.chunk.css"
+  },
+  {
+    "revision": "691c769c04c49370a410",
+    "url": "/weather/static/js/2.c6d919ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.c6d919ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1422cd7d6188d79bd7b2",
+    "url": "/weather/static/js/main.ac5e2d09.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
